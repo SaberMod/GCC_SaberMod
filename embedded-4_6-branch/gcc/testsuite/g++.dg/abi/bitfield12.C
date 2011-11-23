8d9535d00ce6375ac5af8f9d893ff5dd5a6e1c26
jye2 <jye2@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-11-23 03:15:23 +0000

    Partly apply restrict volatile bitfield fix from bernd. 	2010-12-02  Bernd Schmidt  <bernds_cb1@t-online.de>
    	* expr.c (store_field): Avoid a direct store if the mode is larger
    	than the size of the bit field.
    	* stor-layout.c (layout_decl): If flag_strict_volatile_bitfields,
    	treat non-volatile bit fields like volatile ones.
    
    testsuite:
    
    	2011-11-23  Joey Ye  <joey.ye@arm.com>
    	* g++.dg/abi/bitfield12.C: Add option -fno-strict-volatile-bitfields.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ARM@181650 138bc75d-0d04-0410-961f-82ee72b054a4

--- embedded-4_6-branch/gcc/testsuite/g++.dg/abi/bitfield12.C
+++ embedded-4_6-branch/gcc/testsuite/g++.dg/abi/bitfield12.C
@@ -1,4 +1,4 @@
-// { dg-options "-Wabi -fabi-version=1" }
+// { dg-options "-Wabi -fabi-version=1 -fno-strict-volatile-bitfields" }
 
 struct S { // { dg-warning "ABI" }
   char c : 1024; // { dg-warning "width" }
