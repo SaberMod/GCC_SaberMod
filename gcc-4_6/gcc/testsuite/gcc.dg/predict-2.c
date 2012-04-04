0f830096763b7db85e8b6701bb29556b7c0996d8
dehao <dehao@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-04-04 02:10:30 +0000

    revert r185948
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@186130 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_6/gcc/testsuite/gcc.dg/predict-2.c
+++ gcc-4_6/gcc/testsuite/gcc.dg/predict-2.c
@@ -5,20 +5,12 @@ extern int global;
 
 int bar(int);
 
-void foo (int base, int bound)
+void foo (int bound)
 {
   int i, ret = 0;
-  for (i = base; i < bound; i++)
+  for (i = 0; i < bound; i++)
     {
-      if (i > bound * bound)
-	global += bar (i);
-      if (i > bound + 10)
-	global += bar (i);
-      if (i <= bound + 10)
-	global += bar (i);
-      if (i > base + 10)
-	global += bar (i);
-      if (i < base - 10)
+      if (i > bound * bound )
 	global += bar (i);
     }
 }
