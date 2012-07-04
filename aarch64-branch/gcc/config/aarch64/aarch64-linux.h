e7f02d5a174416c724e01cbbbe02a73f9e53bc10
belagod <belagod@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-07-04 17:33:22 +0000

    * config/aarch64/aarch64-linux.h (LINUX_TARGET_LINK_SPEC): Rename 	LINUX_DYNAMIC_LINKER to GLIBC_DYNAMIC_LINKER.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ARM@189265 138bc75d-0d04-0410-961f-82ee72b054a4

--- aarch64-branch/gcc/config/aarch64/aarch64-linux.h
+++ aarch64-branch/gcc/config/aarch64/aarch64-linux.h
@@ -36,7 +36,7 @@
    %{shared:-shared}				\
    %{symbolic:-Bsymbolic}			\
    %{rdynamic:-export-dynamic}			\
-   -dynamic-linker " LINUX_DYNAMIC_LINKER "	\
+   -dynamic-linker " GLIBC_DYNAMIC_LINKER "	\
    -X						\
    %{mbig-endian:-EB} %{mlittle-endian:-EL}"
 
