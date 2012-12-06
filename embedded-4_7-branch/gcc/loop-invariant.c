301ac11035372e3006e1b51dec7120698607aa37
xguo <xguo@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-12-06 03:11:12 +0000

    2012-12-06  Zhenqiang Chen  <zhenqiang.chen@arm.com>
    	* loop-invariant.c (move_loop_invariants): Skip
    	move_single_loop_invariants when optimizing loop for size.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/ARM@194239 138bc75d-0d04-0410-961f-82ee72b054a4

--- embedded-4_7-branch/gcc/loop-invariant.c
+++ embedded-4_7-branch/gcc/loop-invariant.c
@@ -1932,7 +1932,8 @@ move_loop_invariants (void)
       curr_loop = loop;
       /* move_single_loop_invariants for very large loops
 	 is time consuming and might need a lot of memory.  */
-      if (loop->num_nodes <= (unsigned) LOOP_INVARIANT_MAX_BBS_IN_LOOP)
+      if (loop->num_nodes <= (unsigned) LOOP_INVARIANT_MAX_BBS_IN_LOOP
+	  && ! optimize_loop_nest_for_size_p (loop))
 	move_single_loop_invariants (loop);
     }
 
