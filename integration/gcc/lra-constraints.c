0156a68e9dcf139a57973d34044c9e7399448dec
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-02-08 21:26:22 +0000

    Merged revisions 195730-195732,195734-195739,195741-195743,195746-195752,195758-195762,195766,195768,195772,195774,195777,195779-195781,195783-195805,195811-195812,195814-195815,195817-195823,195826,195832,195835-195837,195840-195842,195844-195845,195849-195852,195854-195862,195866-195869,195874,195876-195886,195888,195890-195891,195893,195895-195896 via svnmerge from svn+ssh://gcc.gnu.org/svn/gcc/trunk
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@195901 138bc75d-0d04-0410-961f-82ee72b054a4

--- integration/gcc/lra-constraints.c
+++ integration/gcc/lra-constraints.c
@@ -421,8 +421,20 @@ get_reload_reg (enum op_type type, enum machine_mode mode, rtx original,
       if (rtx_equal_p (curr_insn_input_reloads[i].input, original)
 	  && in_class_p (curr_insn_input_reloads[i].reg, rclass, &new_class))
 	{
-	  *result_reg = curr_insn_input_reloads[i].reg;
-	  regno = REGNO (*result_reg);
+	  rtx reg = curr_insn_input_reloads[i].reg;
+	  regno = REGNO (reg);
+	  /* If input is equal to original and both are VOIDmode,
+	     GET_MODE (reg) might be still different from mode.
+	     Ensure we don't return *result_reg with wrong mode.  */
+	  if (GET_MODE (reg) != mode)
+	    {
+	      if (GET_MODE_SIZE (GET_MODE (reg)) < GET_MODE_SIZE (mode))
+		continue;
+	      reg = lowpart_subreg (mode, reg, GET_MODE (reg));
+	      if (reg == NULL_RTX || GET_CODE (reg) != SUBREG)
+		continue;
+	    }
+	  *result_reg = reg;
 	  if (lra_dump_file != NULL)
 	    {
 	      fprintf (lra_dump_file, "	 Reuse r%d for reload ", regno);
@@ -1895,7 +1907,22 @@ process_alt_operands (int only_alternative)
 			? in_hard_reg_set_p (this_alternative_set,
 					     mode, hard_regno[nop])
 			: in_class_p (op, this_alternative, NULL))))
-		losers++;
+		{
+		  /* Strict_low_part requires to reload the register
+		     not the sub-register.  In this case we should
+		     check that a final reload hard reg can hold the
+		     value mode.  */
+		  if (curr_static_id->operand[nop].strict_low
+		      && REG_P (op)
+		      && hard_regno[nop] < 0
+		      && GET_CODE (*curr_id->operand_loc[nop]) == SUBREG
+		      && ira_class_hard_regs_num[this_alternative] > 0
+		      && ! HARD_REGNO_MODE_OK (ira_class_hard_regs
+					       [this_alternative][0],
+					       GET_MODE (op)))
+		    goto fail;
+		  losers++;
+		}
 	      if (operand_reg[nop] != NULL_RTX
 		  /* Output operands and matched input operands are
 		     not inherited.  The following conditions do not
