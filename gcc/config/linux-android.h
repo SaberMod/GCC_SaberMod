4b308f67cbbb706c30eff06aa484d902f47ba6fd
Paul Beeler <sparksco@gmail.com>
2013-05-08 00:33:45 -0600

    Revert "[4.8.1] Enable x86/arm gcc defaults; Remove march/mtune/mfpmath hardcoded values"
    This reverts commit faa2ae7641fb038167ef0b937d05204072662f56.

--- gcc/config/linux-android.h
+++ gcc/config/linux-android.h
@@ -40,9 +40,9 @@
 #define ANDROID_LINK_SPEC \
   "%{shared: -Bsymbolic} -z noexecstack -z relro -z now"
 
-#define ANDROID_CC1_SPEC(ANDROID_PIC_DEFAULT)				\
+#define ANDROID_CC1_SPEC						\
   "%{!mglibc:%{!muclibc:%{!mbionic: -mbionic}}} "			\
-  "%{!fno-pic:%{!fno-PIC:%{!fpic:%{!fPIC: " ANDROID_PIC_DEFAULT "}}}}"
+  "%{!fno-pic:%{!fno-PIC:%{!fpic:%{!fPIC: -fPIC}}}}"
 
 #define ANDROID_CC1PLUS_SPEC						\
   "%{!fexceptions:%{!fno-exceptions: -fno-exceptions}} "		\
