46220a42995b03e4800b069a7db7a04a08449730
tmsriram <tmsriram@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-07-26 19:02:22 +0000

    Check if target is i?86 or x86_64 before running these tests as they use SSE4.2 target builtins.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@201273 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_8/gcc/testsuite/g++.dg/tree-prof/lipo/target_builtin_0.C
+++ gcc-4_8/gcc/testsuite/g++.dg/tree-prof/lipo/target_builtin_0.C
@@ -1,4 +1,5 @@
 /* Test case to check if LIPO and target specific builtins work fine.  */
+/* { dg-skip-if "" { ! { i?86-*-* x86_64-*-* } } { "*" } { "" } } */
 /* { dg-options "-O2" } */
 
 __attribute__((target("sse4.2")))
