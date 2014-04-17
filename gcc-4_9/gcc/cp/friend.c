671eec7fdcf2e930827cf78f7de5657e18b573a9
ppluzhnikov <ppluzhnikov@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-17 23:14:14 +0000

    For Google b/11542609, backport r209444 from trunk. http://gcc.gnu.org/bugzilla/show_bug.cgi?id=59295
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@209504 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_9/gcc/cp/friend.c
+++ gcc-4_9/gcc/cp/friend.c
@@ -149,7 +149,8 @@ add_friend (tree type, tree decl, bool complain)
 	      if (decl == TREE_VALUE (friends))
 		{
 		  if (complain)
-		    warning (0, "%qD is already a friend of class %qT",
+		    warning (OPT_Wredundant_decls,
+			     "%qD is already a friend of class %qT",
 			     decl, type);
 		  return;
 		}
@@ -377,7 +378,8 @@ make_friend_class (tree type, tree friend_type, bool complain)
 	  if (friend_type == probe)
 	    {
 	      if (complain)
-		warning (0, "%qD is already a friend of %qT", probe, type);
+		warning (OPT_Wredundant_decls,
+			 "%qD is already a friend of %qT", probe, type);
 	      break;
 	    }
 	}
@@ -386,7 +388,8 @@ make_friend_class (tree type, tree friend_type, bool complain)
 	  if (same_type_p (probe, friend_type))
 	    {
 	      if (complain)
-		warning (0, "%qT is already a friend of %qT", probe, type);
+		warning (OPT_Wredundant_decls,
+			 "%qT is already a friend of %qT", probe, type);
 	      break;
 	    }
 	}
