49aea2951917d2e92ce482d4240197793dcc2d99
tejohnson <tejohnson@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-04-23 20:07:34 +0000

    Forward port r197289 from google/gcc-4_7 to support -femit-function-names.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@209722 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_8/gcc/tree-profile.c
+++ gcc-4_8/gcc/tree-profile.c
@@ -1135,6 +1135,9 @@ tree_profiling (void)
 
       push_cfun (DECL_STRUCT_FUNCTION (node->symbol.decl));
 
+      if (flag_emit_function_names)
+        emit_function_name ();
+
       /* Local pure-const may imply need to fixup the cfg.  */
       if (execute_fixup_cfg () & TODO_cleanup_cfg)
 	cleanup_tree_cfg ();
