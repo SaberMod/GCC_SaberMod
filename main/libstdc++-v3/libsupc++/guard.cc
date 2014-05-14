63e56da668e6bd16253ab97427632db8f8fb6573
ppluzhnikov <ppluzhnikov@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-05-14 16:19:58 +0000

    Notify Google runtime about potentially blocking __cxa_guard_acquire.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@210433 138bc75d-0d04-0410-961f-82ee72b054a4

--- main/libstdc++-v3/libsupc++/guard.cc
+++ main/libstdc++-v3/libsupc++/guard.cc
@@ -135,6 +135,26 @@ __set_and_release (__cxxabiv1::__guard *g)
 
 #endif /* __GTHREADS */
 
+
+extern "C" void __google_potentially_blocking_region_begin(void)
+  __attribute__((weak));
+extern "C" void __google_potentially_blocking_region_end(void)
+  __attribute__((weak));
+
+struct google_potentially_blocking_region
+{
+  google_potentially_blocking_region()
+  {
+    if (&__google_potentially_blocking_region_begin != 0)
+      __google_potentially_blocking_region_begin();
+  }
+  ~google_potentially_blocking_region()
+  {
+    if (&__google_potentially_blocking_region_end != 0)
+      __google_potentially_blocking_region_end();
+  }
+};
+
 //
 // Here are C++ run-time routines for guarded initialization of static
 // variables. There are 4 scenarios under which these routines are called:
@@ -230,6 +250,8 @@ namespace __cxxabiv1
   extern "C"
   int __cxa_guard_acquire (__guard *g) 
   {
+    google_potentially_blocking_region gpbr;  // RAII
+
 #ifdef __GTHREADS
     // If the target can reorder loads, we need to insert a read memory
     // barrier so that accesses to the guarded variable happen after the
