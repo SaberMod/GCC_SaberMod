626cf4bd65de3063fb065e2ee56df50658295bbf
wmi <wmi@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-06-19 17:23:55 +0000

    Turn flag_shrink_wrap_frame_pointer off by default temporarily because we still need some support for asynchronous unwinder.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@211825 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_8/gcc/config/i386/i386.c
+++ gcc-4_8/gcc/config/i386/i386.c
@@ -3772,7 +3772,7 @@ ix86_option_override_internal (bool main_args_p)
 
   /* Turn on flag_shrink_wrap_frame_pointer by default.  */
   if (!global_options_set.x_flag_shrink_wrap_frame_pointer)
-    flag_shrink_wrap_frame_pointer = 1;
+    flag_shrink_wrap_frame_pointer = 0;
 
   /* -fshrink-wrap-frame-pointer is an optimization based on
      -fno-omit-frame-pointer mode, so it is only effective when
