a00096f6ea1df63b1b23f340720978a0d9260579
Pavel Chupin <pavel.v.chupin@intel.com>
2014-07-21 22:31:19 +0400

    [4.9] Fix broken ABI defaults for 32-bit x86 compiler
    For multilib compiler x86_64-* we need to pass compiler ABI flags based on
    flags -m32 or -m64, and no flags means -m64. But for 32-bit target compiler
    i686-* we should pass 32-bit ABI flags only. This change fixes the last part.
    
    Change-Id: I00adf4b633952f7a5fde5dc18ca4926a349472cd
    Signed-off-by: Pavel Chupin <pavel.v.chupin@intel.com>
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- gcc-4.9/gcc/config/i386/linux-common.h
+++ gcc-4.9/gcc/config/i386/linux-common.h
@@ -27,11 +27,6 @@ along with GCC; see the file COPYING3.  If not see
     }                                          \
   while (0)
 
-#undef ANDROID_TARGET_CC1_SPEC
-#define ANDROID_TARGET_CC1_SPEC \
-  "%{m32:-mstackrealign -mssse3 -fno-short-enums}" \
-  "%{!m32:-msse4.2 -mpopcnt}"
-
 #undef CC1_SPEC
 #define CC1_SPEC \
   LINUX_OR_ANDROID_CC (GNU_USER_TARGET_CC1_SPEC, \
