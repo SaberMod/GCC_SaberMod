b5f07a58046fd41dde7777d51d4a13b58cfb2510
xur <xur@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-08-06 00:44:26 +0000

    2014-08-05  Rong Xu  <xur@google.com>
    	* gcc/config/aarch64/aarch64.c (aarch64_override_options):
            Add aarch64 target specific options for android.
    
    Index: gcc/config/aarch64/aarch64.c
    ===================================================================
    --- gcc/config/aarch64/aarch64.c	(revision 213641)
    +++ gcc/config/aarch64/aarch64.c	(working copy)
    @@ -5257,6 +5257,20 @@ aarch64_override_options (void)
       aarch64_tune_params = selected_tune->tune;
     
       aarch64_override_options_after_change ();
    +
    +  if (TARGET_ANDROID)
    +    {
    +      /* Lower the complete unroll code size limits.
    +         Loop unroll needs some tuning in arm and aarch64.  */
    +      maybe_set_param_value (PARAM_MAX_DEFAULT_COMPLETELY_PEELED_INSNS, 50,
    +                             global_options.x_param_values,
    +                             global_options_set.x_param_values);
    +
    +      /* Disable array_bound warning. Work around isses
    +         introduced in complete unroll.  */
    +      global_options.x_warn_array_bounds = 0;
    +    }
    +
     }
     
     /* Implement targetm.override_options_after_change.  */
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@213650 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_9/gcc/config/aarch64/aarch64.c
+++ gcc-4_9/gcc/config/aarch64/aarch64.c
@@ -5257,6 +5257,20 @@ aarch64_override_options (void)
   aarch64_tune_params = selected_tune->tune;
 
   aarch64_override_options_after_change ();
+
+  if (TARGET_ANDROID)
+    {
+      /* Lower the complete unroll code size limits.
+         Loop unroll needs some tuning in arm and aarch64.  */
+      maybe_set_param_value (PARAM_MAX_DEFAULT_COMPLETELY_PEELED_INSNS, 50,
+                             global_options.x_param_values,
+                             global_options_set.x_param_values);
+
+      /* Disable array_bound warning. Work around isses
+         introduced in complete unroll.  */
+      global_options.x_warn_array_bounds = 0;
+    }
+
 }
 
 /* Implement targetm.override_options_after_change.  */
