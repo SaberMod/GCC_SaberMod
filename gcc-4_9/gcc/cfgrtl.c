230df0d6302ea5123bb3bdcc287e4e273113c3ab
xur <xur@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-10-22 20:38:42 +0000

    2014-10-22    <xur@google.com>         Google ref b/17759776.         * cfgrtl.c (emit_barrier_after_bb): Append the barrier to the         footer, instead of unconditionally overwritten.         * testsuite/g++.dg/tree-prof/pr63581.C: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@216565 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_9/gcc/cfgrtl.c
+++ gcc-4_9/gcc/cfgrtl.c
@@ -1453,7 +1453,24 @@ emit_barrier_after_bb (basic_block bb)
   gcc_assert (current_ir_type () == IR_RTL_CFGRTL
               || current_ir_type () == IR_RTL_CFGLAYOUT);
   if (current_ir_type () == IR_RTL_CFGLAYOUT)
-    BB_FOOTER (bb) = unlink_insn_chain (barrier, barrier);
+    {
+      rtx insn = unlink_insn_chain (barrier, barrier);
+
+      if (BB_FOOTER (bb))
+        { 
+          rtx footer_tail = BB_FOOTER (bb);
+
+          while (NEXT_INSN (footer_tail))
+            footer_tail = NEXT_INSN (insn);
+          if (!BARRIER_P (footer_tail))
+            {
+              NEXT_INSN (footer_tail) = insn;
+              PREV_INSN (insn) = footer_tail;
+            }
+        }
+      else
+        BB_FOOTER (bb) = insn;
+    }
 }
 
 /* Like force_nonfallthru below, but additionally performs redirection
