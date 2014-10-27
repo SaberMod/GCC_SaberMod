c08c92d501b55bed27e028c301fd6041280d6986
Rong Xu <xur@google.com>
2014-10-27 13:09:03 -0700

    [4.9] Backport r216764: Enable ifunc for BINOIC Backport r216764 from google/gcc-4_9 branches which makes ifunc support available for BIONIC (still controlled by configure).
    Change-Id: I515d4d38e11bd0378899e64e68ab5b255e2c6df3
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- gcc/config/linux.c
+++ gcc/config/linux.c
@@ -28,7 +28,7 @@ along with GCC; see the file COPYING3.  If not see
 bool
 linux_has_ifunc_p (void)
 {
-  return OPTION_BIONIC ? false : HAVE_GNU_INDIRECT_FUNCTION;
+  return HAVE_GNU_INDIRECT_FUNCTION;
 }
 
 bool
