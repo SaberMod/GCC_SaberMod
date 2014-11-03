a835f743147f97d6c2a74f02e8d5491237c824eb
Paul Beeler <pbeeler80@gmail.com>
2014-11-02 20:42:27 -0700

    Fix arm_neon.h
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- gcc/config/arm/arm_neon.h
+++ gcc/config/arm/arm_neon.h
@@ -31,6 +31,10 @@
 extern "C" {
 #endif
 
+#ifndef __ARM_NEON__
+#error You must enable NEON instructions (e.g. -mfloat-abi=softfp -mfpu=neon) to use arm_neon.h
+#else
+
 #include <stdint.h>
 
 typedef __builtin_neon_qi int8x8_t	__attribute__ ((__vector_size__ (8)));
@@ -13808,5 +13812,6 @@ vmull_high_p64 (poly64x2_t __a, poly64x2_t __b)
 #endif
 #ifdef __cplusplus
 }
+#endif
 #endif
 #endif
