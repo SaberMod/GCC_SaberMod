8c91c218e50c250efd21dcbe7b7f943ac547c664
tmsriram <tmsriram@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-12-23 19:32:51 +0000

    Revert rev. 212437 to remove previous patch that added mcopyrelocs.
    Merge trunk rev. 218397 and rev.218408 that adds this as
    the default with -fPIE after checking if the linker supports
    copyrelocations with PIE.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@219048 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_9/gcc/testsuite/gcc.target/i386/pie-copyrelocs-1.c
+++ gcc-4_9/gcc/testsuite/gcc.target/i386/pie-copyrelocs-1.c
@@ -1,6 +1,7 @@
-/* Test if -mcopyrelocs does the right thing. */
-/* { dg-do compile } */
-/* { dg-options "-O2 -fpie -mcopyrelocs" } */
+/* Check that GOTPCREL isn't used to access glob_a.  */
+/* { dg-do compile { target *-*-linux* } } */
+/* { dg-require-effective-target pie_copyreloc } */
+/* { dg-options "-O2 -fpie" } */
 
 extern int glob_a;
 
@@ -9,5 +10,5 @@ int foo ()
   return glob_a;
 }
 
-/* glob_a should never be accessed with a GOTPCREL  */ 
-/* { dg-final { scan-assembler-not "glob_a\\@GOTPCREL" { target { x86_64-*-* } } } } */
+/* glob_a should never be accessed with a GOTPCREL.  */
+/* { dg-final { scan-assembler-not "glob_a@GOTPCREL" { target { ! ia32 } } } } */
