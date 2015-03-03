2d017af59333cc62b5665730fb3e95ee22668002
ccoutant <ccoutant@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-03-03 21:17:34 +0000

    More minor changes for -ftwo-level-line-tables.
    With this patch, we output an empty name for non-inlined functions,
    and change the -ftwo-level-all-subprogs option to override this
    behavior to output linkage names instead. The consumer can obtain
    the linkage name for non-inlined functions from the ELF symbol table,
    so we can save quite a bit of space in the line tables by omitting
    those names.
    
    This patch also fixes a case where we output the linkage name for
    a clone rather than for the ultimate origin.
    
    This patch is for the google/gcc-4_9 branch.
    
    2015-03-03  Cary Coutant  <ccoutant@google.com>
    
    	* common.opt (ftwo-level-all-subprogs): Default to off;
            update help text.
    	* dwarf2out.c (add_subprog_entry): Clear subprog_num if the
            subprogram is inlined, but was already output as non-inlined.
    	(out_subprog_directive): Get name from decl_ultimate_origin.
            Output empty name for non-inlined subprograms.
    	(out_logical_entry): Output subprog entries for all subprograms.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google@221163 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc-4_9/gcc/dwarf2out.c
+++ gcc-4_9/gcc/dwarf2out.c
@@ -21506,8 +21506,14 @@ add_subprog_entry (tree decl, bool is_inlined)
       entry->subprog_num = 0;
       *slot = entry;
     }
-  else if (is_inlined)
-    (*slot)->is_inlined = true;
+  else if (is_inlined && !(*slot)->is_inlined)
+    {
+      /* If we've already output this subprogram entry as a non-inlined
+         subprogram, make sure it gets output again, so that we include
+         its linkage name.  */
+      (*slot)->is_inlined = true;
+      (*slot)->subprog_num = 0;
+    }
   return *slot;
 }
 
@@ -21817,22 +21823,39 @@ out_subprog_directive (subprog_entry *subprog)
 {
   tree decl = subprog->decl;
   tree decl_name = DECL_NAME (decl);
-  const char *name;
+  tree origin = NULL_TREE;
+  const char *name = NULL;
   unsigned int file_num = 0;
   unsigned int line_num = 0;
 
   if (decl_name == NULL || IDENTIFIER_POINTER (decl_name) == NULL)
     return;
 
-  /* For inlined subroutines, use the linkage name.  */
-  if (subprog->is_inlined && DECL_ASSEMBLER_NAME (decl))
+  origin = decl_ultimate_origin (decl);
+  if (origin == NULL_TREE)
+    origin = decl;
+
+  /* For inlined subroutines, use the linkage name.
+     If -ftwo-level-all-subprogs is set, use the linkage name
+     for all subroutines.  */
+  if (subprog->is_inlined || flag_two_level_all_subprogs)
     {
-      name = IDENTIFIER_POINTER (DECL_ASSEMBLER_NAME (decl));
-      if (name[0] == '*')
-        name++;
+      if (DECL_ASSEMBLER_NAME (origin))
+	{
+	  name = IDENTIFIER_POINTER (DECL_ASSEMBLER_NAME (origin));
+	  if (name[0] == '*')
+	    name++;
+	}
+      else
+	name = dwarf2_name (origin, 0);
     }
   else
-    name = dwarf2_name (decl, 0);
+    {
+      /* To save space, we don't emit the name for non-inlined
+         subroutines, whose linkage names are available from the
+         object file's symbol table.  */
+      name = "";
+    }
 
   if (LOCATION_LOCUS (DECL_SOURCE_LOCATION (decl)) != UNKNOWN_LOCATION)
     {
@@ -21881,8 +21904,7 @@ out_logical_entry (dw_line_info_table *table, unsigned int file_num,
   /* Declare the subprogram if it hasn't already been declared.  */
   if (block != NULL)
     subprog = block->subprog;
-  if (subprog != NULL && subprog->subprog_num == 0
-      && (context != NULL || flag_two_level_all_subprogs))
+  if (subprog != NULL && subprog->subprog_num == 0)
     out_subprog_directive (subprog);
   if (subprog != NULL)
     subprog_num = subprog->subprog_num;
