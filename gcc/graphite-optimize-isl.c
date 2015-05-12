f1d8424ae4a3c9ece263385a8acd803221bd8291
Paul Beeler <pbeeler80@gmail.com>
2015-05-11 22:27:25 -0600

    Graphite: Add missing headers for isl-0.15
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- gcc/graphite-optimize-isl.c
+++ gcc/graphite-optimize-isl.c
@@ -28,6 +28,7 @@ along with GCC; see the file COPYING3.  If not see
 #include <isl/band.h>
 #include <isl/aff.h>
 #include <isl/options.h>
+#include <isl/union_set.h>
 #ifdef HAVE_ISL_SCHED_CONSTRAINTS_COMPUTE_SCHEDULE
 #include <isl/deprecated/int.h>
 #include <isl/deprecated/aff_int.h>
