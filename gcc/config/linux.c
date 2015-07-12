3132346c76ffd4d413bc75910cbc01a23d8c7b0d
Andrew Hsieh <andrewhsieh@google.com>
2015-07-12 02:09:12 -0400

    [4.9] Disable sincos
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- gcc/config/linux.c
+++ gcc/config/linux.c
@@ -39,7 +39,6 @@ linux_libc_has_function (enum function_class fn_class)
   if (OPTION_BIONIC)
     if (fn_class == function_c94
 	|| fn_class == function_c99_misc
-	|| fn_class == function_sincos)
 	return true;
 
   return false;
