655515a4c961b4a4f9e5873cef27535ed197f261
rguenth <rguenth@138bc75d-0d04-0410-961f-82ee72b054a4>
2015-10-12 10:59:21 +0000

    Backported by Paul Beeler
    2015-10-12  Richard Biener  <rguenther@suse.de>
    
    	Backport from mainline
    	2015-07-21  Mike Frysinger  <vapier@gentoo.org>
    		    Bernhard Reutner-Fischer  <aldot@gcc.gnu.org>
    
    	* configure.ac: Add check for new options in isl-0.15.
    	* config.in, configure: Rebuilt.
    	* graphite-blocking.c: Include <isl/constraint.h>
    	* graphite-interchange.c,  graphite-poly.c: Likewise.
    	* graphhite-scop-detection.c, graphite-sese-to-poly.c: Likewise.
    	* graphite.c: Likewise.
    	* graphite-isl-ast-to-gimple.c: Include <isl/constraint.h> and
    	<isl/union_set.h>.
    	* graphite-dependences.c: Include <isl/constraint.h>.
    	(max_number_of_out_dimensions): Returns isl_stat.
    	(extend_schedule_1): Likewise
    	(extend_schedule): Corresponding changes.
    	* graphite-optimize-isl.c: Include <isl/constraint.h> and
    	<isl/union_set.h>.
    	(getSingleMap): Change return type of isl_stat.
    	(optimize_isl): Conditionally use
    	isl_options_set_schedule_serialize_sccs.
    	* graphite-poly.h (isl_stat, isl_stat_ok): Define fallbacks
    	if not HAVE_ISL_OPTIONS_SET_SCHEDULE_SERIALIZE_SCCS.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/gcc-5-branch@228708 138bc75d-0d04-0410-961f-82ee72b054a4
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- gcc/graphite-poly.c
+++ gcc/graphite-poly.c
@@ -25,7 +25,6 @@ along with GCC; see the file COPYING3.  If not see
 #include <isl/set.h>
 #include <isl/map.h>
 #include <isl/union_map.h>
-#include <isl/constraint.h>
 #include <isl/ilp.h>
 #include <isl/aff.h>
 #include <isl/val.h>
